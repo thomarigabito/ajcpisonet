e2a3427bb054d8cc9b46bb4dce083ef57666f6b9
juki3r <deepong25@gmail.com>
2024-02-13 14:57:13 +0800

    modified:   resources/views/layouts/guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,3 +1,6 @@
+@extends('mainlayout')
+@section('title', 'AJC-Login')
+
 @section('content')
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
