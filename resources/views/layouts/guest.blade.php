77591ea2f422c218682844079fc7ef8182523c11
juki3r <deepong25@gmail.com>
2024-02-13 19:36:37 +0800

    modified:   resources/views/layouts/guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,9 +2,9 @@
 @section('title', 'AJC-Login')
 
 @section('content')
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-2 sm:pt-0 bg-gray-100">
-            <div class="w-full sm:max-w-md mt-3 px-6 bg-white py-4 shadow-md overflow-hidden sm:rounded-lg">
+        {{-- <div class="min-h-screen flex flex-col sm:justify-center items-center pt-2 sm:pt-0 bg-gray-100">
+            <div class="w-full sm:max-w-md mt-3 px-6 bg-white py-4 shadow-md overflow-hidden sm:rounded-lg"> --}}
                 {{ $slot }}
-            </div>
-        </div>
+            {{-- </div>
+        </div> --}}
 @endsection
