e2a6a6ec708a6ed43a09a84c1bb46ff851c5a0e8
juki3r <deepong25@gmail.com>
2024-02-13 19:43:22 +0800

    modified:   resources/views/auth/login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,7 @@
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
+        <h3>Login</h3>
         @csrf
 
         <!-- Email Address -->
