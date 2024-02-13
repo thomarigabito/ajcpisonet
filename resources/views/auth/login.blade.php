44ef4f0f094b9c3b82e5049ee2af292e1a588e93
juki3r <deepong25@gmail.com>
2024-02-13 15:27:22 +0800

    modified:   resources/views/auth/login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                 </a>
             @endif
 
-            <x-primary-button class="ms-3 text-primary">
+            <x-primary-button class="ms-3 " style="color:black">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
