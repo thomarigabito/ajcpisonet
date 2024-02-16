0525ebfb150528d72c716545d9ca7e7933483fb4
juki3r <deepong25@gmail.com>
2024-02-16 18:59:47 +0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::get('/', [AJCController::class, 'homepage']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::get('/admindashboard', [DashboardController::class, 'dashboard'])->name('admindashboard');
     Route::get('/addClient', [ClientsController::class, 'addClient'])->name('addClient');
     Route::get('/bill', [AJCController::class, 'bill'])->name('bill');
     
