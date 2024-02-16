1ea11e792abb1e337f52170ff79fd6cd7384d8bb
juki3r <deepong25@gmail.com>
2024-02-16 19:56:46 +0800

    modified:   app/Http/Controllers/AJCController.php

--- app/Http/Controllers/AJCController.php
+++ app/Http/Controllers/AJCController.php
@@ -11,35 +11,55 @@ class AJCController extends Controller
 
     public function homepage(){
         if(Auth::check()){
-            return redirect(route('dashboard'));
+            if(Auth::user()->usertype=='admin'){
+                return redirect(route('admindashboard'));
+            }else{
+                return redirect(route('dashboard'));
+            }   
         }
         return view('homepage');
     }
     public function internet()
     {
         if(Auth::check()){
-            return redirect(route('dashboard'));
+            if(Auth::user()->usertype=='admin'){
+                return redirect(route('admindashboard'));
+            }else{
+                return redirect(route('dashboard'));
+            } 
         }
         return view('include.internet');
     }
     public function promos()
     {
         if(Auth::check()){
-            return redirect(route('dashboard'));
+            if(Auth::user()->usertype=='admin'){
+                return redirect(route('admindashboard'));
+            }else{
+                return redirect(route('dashboard'));
+            } 
         }
         return view('include.promos');
     }
     public function contactus()
     {
         if(Auth::check()){
-            return redirect(route('dashboard'));
+            if(Auth::user()->usertype=='admin'){
+                return redirect(route('admindashboard'));
+            }else{
+                return redirect(route('dashboard'));
+            } 
         }
         return view('include.contactus');
     }
     public function applynow()
     {
         if(Auth::check()){
-            return redirect(route('dashboard'));
+            if(Auth::user()->usertype=='admin'){
+                return redirect(route('admindashboard'));
+            }else{
+                return redirect(route('dashboard'));
+            } 
         }
         return view('include.applynow');
     }
@@ -47,7 +67,11 @@ class AJCController extends Controller
 
     function applynowPOST(Request $request){
         if(Auth::check()){
-            return redirect(route('dashboard'));
+            if(Auth::user()->usertype=='admin'){
+                return redirect(route('admindashboard'));
+            }else{
+                return redirect(route('dashboard'));
+            } 
         }
 
 
