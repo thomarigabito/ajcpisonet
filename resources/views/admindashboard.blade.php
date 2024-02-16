7038da1f0bf29fc5301631d1048f3147c7ca0c68
juki3r <148548055+juki3r@users.noreply.github.com>
2024-02-16 16:45:03 +0800

    new

--- resources/views/admindashboard.blade.php
+++ resources/views/admindashboard.blade.php
@@ -10,6 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h1>ADMIN DASHBOARD</h1>
+                    {{Auth::user()->name}}
                 </div>
             </div>
         </div>
