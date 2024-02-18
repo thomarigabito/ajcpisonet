010699d717a8f4d9262169d81dad1d44b4ecc8ad
juki3r <deepong25@gmail.com>
2024-02-18 22:12:26 +0800

    modified:   resources/views/include/index.blade.php 	modified:   routes/web.php

--- resources/views/include/index.blade.php
+++ resources/views/include/index.blade.php
@@ -1,7 +1,4 @@
 <x-app-layout>
-    <x-slot name="title">
-        Clients
-    </x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
