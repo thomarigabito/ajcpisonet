453454be1f4fa2d794117c1c5fe97646d9728a1f
juki3r <deepong25@gmail.com>
2024-02-18 21:12:29 +0800

    modified:   database/migrations/2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('usertype')->default('user'); 
-            $table->int('accountNumber');
+            $table->integer('accountNumber');
             $table->rememberToken();
             $table->timestamps();
         });
