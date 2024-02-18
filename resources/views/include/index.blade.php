64ed32a47698a0fddcfbb473623a6e65eb70ea83
juki3r <deepong25@gmail.com>
2024-02-18 23:55:03 +0800

    modified:   resources/views/include/index.blade.php

--- resources/views/include/index.blade.php
+++ resources/views/include/index.blade.php
@@ -9,40 +9,42 @@
         <div class="max-w-10xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                   <div class="card table-responsive">
+                   <div class="card">
                     <div class="card-header">
                         <h4>Clients
                             <a href="{{route('createclient')}}" class="btn btn-primary float-end">Add Client</a>
                         </h4>
                     </div>
                     <div class="card-body">
-                        <table class="table table-bordered table-striped">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Fullname</th>
-                                    <th>Address</th>
-                                    <th>Plan</th>
-                                    <th>Account#</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($allclientdata as $item)
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped">
+                                <thead>
                                     <tr>
-                                        <td>{{$item->id}}</td>
-                                        <td>{{$item->fullname}}</td>
-                                        <td>{{$item->address}}</td>
-                                        <td>{{$item->plan}}</td>
-                                        <td>{{$item->accountNumber}}</td>
-                                        <td>
-                                            <a href="{{url('clients/'.$item->id.'/edit')}}" class="btn btn-success mx-3">Edit</a>
-                                            <a href="{{url('clients/'.$item->id.'/delete')}}" class="btn btn-danger mx-1" onclick="return confirm('Are you sure?')">Delete</a>
-                                        </td>
+                                        <th>ID</th>
+                                        <th>Fullname</th>
+                                        <th>Address</th>
+                                        <th>Plan</th>
+                                        <th>Account#</th>
+                                        <th>Action</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($allclientdata as $item)
+                                        <tr>
+                                            <td>{{$item->id}}</td>
+                                            <td>{{$item->fullname}}</td>
+                                            <td>{{$item->address}}</td>
+                                            <td>{{$item->plan}}</td>
+                                            <td>{{$item->accountNumber}}</td>
+                                            <td>
+                                                <a href="{{url('clients/'.$item->id.'/edit')}}" class="btn btn-success mx-3">Edit</a>
+                                                <a href="{{url('clients/'.$item->id.'/delete')}}" class="btn btn-danger mx-1" onclick="return confirm('Are you sure?')">Delete</a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                    </div>
                    
