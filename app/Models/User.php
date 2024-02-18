abede48db3a67b2e8e3facce64885de2f82d50ec
juki3r <deepong25@gmail.com>
2024-02-18 21:11:53 +0800

    modified:   app/Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,8 +22,7 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $fillable = [
         'name',
         'email',
-        'password',
-        'type'
+        'password'
     ];
  
     protected $hidden = [
