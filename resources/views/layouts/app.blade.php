1d57f4bff3f267e146fd6f70dbf538ec4fd50754
juki3r <deepong25@gmail.com>
2024-02-19 15:25:37 +0800

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'AJC Pisonet') }}</title>
+        <link rel="icon" type="image/x-icon" href="/assets/agclogo.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
