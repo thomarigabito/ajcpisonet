6092ba4a55ac01b196766a4edf05d18bbeead7b7
juki3r <deepong25@gmail.com>
2024-02-19 17:12:15 +0800

    modified:   resources/views/include/editclient.blade.php

--- resources/views/include/editclient.blade.php
+++ resources/views/include/editclient.blade.php
@@ -1,13 +1,5 @@
-@extends('mainlayout')
 
-
-
-
-
-
-
-
-{{-- <x-app-layout>
+<x-app-layout>
   <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Update Client') }}
@@ -63,4 +55,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>  --}}
+</x-app-layout> 
