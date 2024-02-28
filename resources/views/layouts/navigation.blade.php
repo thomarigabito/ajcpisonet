69909d9f9c7f9773cb6f766cb30be34b77731616
juki3r <deepong25@gmail.com>
2024-02-28 22:32:51 +0800

    modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,11 +38,11 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    @if(Auth::user()->accountNumber)
+                    
                     <x-nav-link :href="route('bill')" :active="request()->routeIs('bill')">
                         {{ __('Bill') }}
                     </x-nav-link>
-                    @endif
+                    
                 </div>
                 @endif
 
