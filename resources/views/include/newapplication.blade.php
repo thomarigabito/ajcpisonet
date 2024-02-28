e82a521a9bb6b28bb5062eba6b011abd8e779825
juki3r <deepong25@gmail.com>
2024-02-29 07:38:39 +0800

    modified:   resources/views/include/newapplication.blade.php

--- resources/views/include/newapplication.blade.php
+++ resources/views/include/newapplication.blade.php
@@ -68,7 +68,7 @@
                                                                 $applicants->street,
                                                                 $applicants->barangay,
                                                                 $applicants->town,
-                                                                applicants->province
+                                                                $applicants->province
                                                             ])}}" 
                                                             class="btn btn-success mx-1" >
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
