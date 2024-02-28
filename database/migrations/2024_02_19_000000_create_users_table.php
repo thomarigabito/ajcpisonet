59fc6b1561c551d148614412cef4805f56745cfc
juki3r <deepong25@gmail.com>
2024-02-28 23:18:17 +0800

    modified:   database/migrations/2024_02_19_000000_create_users_table.php

--- database/migrations/2024_02_19_000000_create_users_table.php
+++ database/migrations/2024_02_19_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('usertype')->default('user');
-            $table->string('accountnumber')->nullable();
+            $table->string('accountnumber')->nullable()->primary();
             $table->foreign('accountnumber')->references('accountNumber')->on('allclients');
             // ->onDelete('cascade');
             $table->rememberToken();
