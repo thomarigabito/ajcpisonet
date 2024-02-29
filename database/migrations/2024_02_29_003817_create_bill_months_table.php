68afbc7af0319892ea5765897930febaf573ca85
juki3r <deepong25@gmail.com>
2024-02-29 08:54:25 +0800

    modified:   database/migrations/2024_02_29_003817_create_bill_months_table.php

--- database/migrations/2024_02_29_003817_create_bill_months_table.php
+++ database/migrations/2024_02_29_003817_create_bill_months_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('bill_months', function (Blueprint $table) {
             $table->unsignedBigInteger('id');
             $table->string('March');
-            
             $table->timestamps();
         });
     }
