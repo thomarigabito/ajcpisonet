5873d5c05c7969aea33be8d2230dd2cef7e7fa6a
juki3r <deepong25@gmail.com>
2024-02-29 08:58:28 +0800

    modified:   database/migrations/2024_02_10_003817_create_bill_months_table.php

--- database/migrations/2024_02_10_003817_create_bill_months_table.php
+++ database/migrations/2024_02_10_003817_create_bill_months_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('bill_months', function (Blueprint $table) {
-            $table->unsignedBigInteger('id');
+            $table->id();
             $table->string('March');
             $table->timestamps();
         });
