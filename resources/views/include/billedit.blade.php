b0bbea6b36418618d79c79667eb39416e7d854dc
juki3r <deepong25@gmail.com>
2024-03-02 22:54:41 +0800

    modified:   resources/views/include/billedit.blade.php

--- resources/views/include/billedit.blade.php
+++ resources/views/include/billedit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{$BillEdit->fullname .'Billing Update' }}
+            {{$BillEdit->fullname .' Billing Update' }}
         </h2>
     </x-slot>
 
