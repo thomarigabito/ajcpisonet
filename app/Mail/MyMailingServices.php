58d7dbf750e32bba912dbda985a3bbc5a51db27d
juki3r <deepong25@gmail.com>
2024-03-03 11:53:33 +0800

    modified:   app/Mail/MyMailingServices.php 	modified:   routes/web.php

--- app/Mail/MyMailingServices.php
+++ app/Mail/MyMailingServices.php
@@ -16,7 +16,7 @@ class MyMailingServices extends Mailable
     /**
      * Create a new message instance.
      */
-    public function __construct(private $fullname, $accountnumber)
+    public function __construct(private $fullname, $accountnumber, $email)
     {
         //
     }
