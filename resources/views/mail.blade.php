538c15be5347c9a979ec6d49459e3f882b5d911a
juki3r <deepong25@gmail.com>
2024-03-03 12:48:11 +0800

    modified:   resources/views/mail.blade.php

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -3,8 +3,8 @@
 
 <h2>Hi <strong style="text-transform: capitalize">{{$fullname}}</strong>,</h2>
 <p class="my-2">Below is the summary of your monthly billing:</p><br/>
-<p>Account Number : {{accountnumber}}</p><br/>
-<p>Acoount Name : {{fullname}}</p><br/>
+<p>Account Number : {{$accountnumber}}</p><br/>
+<p>Acoount Name : {{$fullname}}</p><br/>
 <p>Current Charges : 1500</p><br/>
 
 
