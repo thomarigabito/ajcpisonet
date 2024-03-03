2f26fb3bbbcacefdd81c0ed41d59b96a7cb67ee4
juki3r <deepong25@gmail.com>
2024-03-03 11:10:54 +0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -65,10 +65,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     // To be customize
     Route::get('clients/{email}/{fullname}', function($email, $fullname){
         //Send email to client Bill Reminder
-        // Mail::to($email)->send (new MyMailingServices($fullname));
-        // return redirect()->back()->with('status', 'Bill reminder email sent');
-        echo $email;
-        echo $fullname;
+        Mail::to($email)->send (new MyMailingServices($fullname));
+        return redirect()->back()->with('status', 'Bill reminder email sent');
     })->name('sendemail');
 });
 
