e660d60551a0a4e1e502b1018fa4914090a17ff9
juki3r <deepong25@gmail.com>
2024-03-04 22:20:36 +0800

    modified:   app/Http/Controllers/DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -39,7 +39,7 @@ class DashboardController extends Controller
        
 
        
-        return view('dashboard', [$accountnumber]);
+        return view('dashboard', ['accountnumber' => $accountnumber]);
         
         
 
