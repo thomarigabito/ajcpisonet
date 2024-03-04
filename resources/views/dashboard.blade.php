0e9d999bd240120b6ba83f9b9c523788a2f16131
juki3r <deepong25@gmail.com>
2024-03-04 22:31:36 +0800

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,7 @@
 
 
         <div class="py-12">
-            <div class="max-w-10xl mx-auto sm:px-2 lg:px-2">
+            <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 align-items-end">
 
