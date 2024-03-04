66d0eae9fed25bf0aa22ccb2a78b1e47355fb704
juki3r <deepong25@gmail.com>
2024-03-04 22:29:47 +0800

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,7 @@
 
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-2">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 align-items-end">
 
@@ -72,5 +72,5 @@
 
     @endif
 
-    @include('footer')
+    {{-- @include('footer') --}}
 </x-app-layout>
