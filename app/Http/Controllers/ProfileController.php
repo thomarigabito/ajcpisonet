bf4f04e30fa35131f4476e5908fd0af204bde9a2
juki3r <deepong25@gmail.com>
2024-03-08 12:43:18 +0800

    modified:   app/Http/Controllers/ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -52,7 +52,7 @@ class ProfileController extends Controller
         };
 
 
-        // $request->user()->save();
+        $request->user()->save();
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
