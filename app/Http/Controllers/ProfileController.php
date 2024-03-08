3da7e1904ab1fd01851faca3c2c86f01dead5a2d
juki3r <deepong25@gmail.com>
2024-03-08 12:11:25 +0800

    modified:   app/Http/Controllers/ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -31,7 +31,7 @@ class ProfileController extends Controller
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
         }
-        dd($validate);
+        dd($request->validate());
         if($request->has('profilepicture')){
             $imagePath = $request->profilepicture->file('profilepicture')->store('profile', 'public');
             $request->profilepicture = $imagePath;
